fd2fd7abe5ff4897778f6093ad8d69a16327cf99
Marcus Rugger <dev@marcusrugger.com>
2024-04-30 09:59:58 -0400

    add dark flavor

--- lua/catppuccin/init.lua
+++ lua/catppuccin/init.lua
@@ -100,7 +100,7 @@ local M = {
 		color_overrides = {},
 		highlight_overrides = {},
 	},
-	flavours = { latte = 1, frappe = 2, macchiato = 3, mocha = 4 },
+	flavours = { latte = 1, frappe = 2, macchiato = 3, mocha = 4, dark = 5 },
 	path_sep = jit and (jit.os == "Windows" and "\\" or "/") or package.config:sub(1, 1),
 }
 
@@ -126,7 +126,7 @@ local function get_flavour(default)
 	if flavour and not M.flavours[flavour] then
 		vim.notify(
 			string.format(
-				"Catppuccin (error): Invalid flavour '%s', flavour must be 'latte', 'frappe', 'macchiato', 'mocha' or 'auto'",
+				"Catppuccin (error): Invalid flavour '%s', flavour must be 'latte', 'frappe', 'macchiato', 'mocha', 'dark', or 'auto'",
 				flavour
 			),
 			vim.log.levels.ERROR
